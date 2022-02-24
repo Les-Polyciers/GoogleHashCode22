8e8e2ec97d87eb74ddd1fbb3468891c30e8f0f4e
Rémi cazin <remi.cazin@polyconseil.fr>
2022-02-24 19:34:59 +0100

    Add example

--- src/main/java/business/SolverImpl.java
+++ src/main/java/business/SolverImpl.java
@@ -1,9 +1,20 @@
 package business;
 
+import business.dto.output.OContributor;
+import business.dto.output.OProject;
+
+import java.util.List;
+
 public class SolverImpl implements Solver {
 
   @Override
   public ProblemOutputParameters solve(ProblemInputParameters inputParameters) {
-    // TODO
+    OContributor bob = new OContributor("Bob");
+    OContributor ana = new OContributor("Anna");
+    OContributor maria = new OContributor("Maria");
+    OProject webserver = new OProject("WebServer", List.of(bob, ana));
+    OProject logging = new OProject("Logging", List.of(ana));
+    OProject webchat = new OProject("WebChat", List.of(maria, bob));
+    return new ProblemOutputParameters(List.of(webserver, logging, webchat));
   }
 }
