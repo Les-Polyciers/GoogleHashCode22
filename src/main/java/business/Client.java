1c1182237fd6bb88099565dd57f29f8f87dc19a6
FlorianCousin <florian.cousin@polyconseil.fr>
2022-02-24 08:23:35 +0100

    Ignore number of ingredients

--- src/main/java/business/Client.java
+++ src/main/java/business/Client.java
@@ -2,7 +2,9 @@ package business;
 
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 public class Client {
@@ -11,7 +13,11 @@ public class Client {
   private final Set<String> dislikedIngredients;
 
   public Client(String inputLikes, String inputDislikes) {
-    this.likedIngredients = Set.of(inputLikes.split(" "));
-    this.dislikedIngredients = Set.of(inputDislikes.split(" "));
+
+    this.likedIngredients =
+        Arrays.stream(inputLikes.split(" ")).skip(1).collect(Collectors.toUnmodifiableSet());
+
+    this.dislikedIngredients =
+        Arrays.stream(inputDislikes.split(" ")).skip(1).collect(Collectors.toUnmodifiableSet());
   }
 }
