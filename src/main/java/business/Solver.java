be5a942ce62e268ead6e755d27f5dc6412c77aa3
FlorianCousin <florian.cousin@polyconseil.fr>
2022-02-24 19:23:56 +0100

    Remove classes

--- src/main/java/business/Solver.java
+++ src/main/java/business/Solver.java
@@ -6,8 +6,6 @@ import lombok.experimental.UtilityClass;
 public final class Solver {
 
   public static ProblemOutputParameters solve(ProblemInputParameters inputParameters) {
-    Client anyClient = inputParameters.getClients().stream().findAny().orElseThrow();
-    Pizza pizza = new Pizza(anyClient.getLikedIngredients());
-    return new ProblemOutputParameters(pizza);
+    // TODO
   }
 }
